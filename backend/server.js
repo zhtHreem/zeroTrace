cfe4a3615d9859e366e29f12f820af0c0cd683ec
Hareem <zainabhareemtayyab@gmail.com>
2024-11-18 03:05:29 +0500

    added response form path

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import { dirname } from 'path';
 import FormData from 'form-data';
 //import ProductRoutes from './src/database/models/Product/route.js';
 import FormRoute from './src/database/models/Form/route.js'
+import ResponseForm from './src/database/models/ResponseForm/route.js';
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,6 +16,8 @@ const port = process.env.PORT || 5000;
 connectDB();
 console.log("OK")
 app.use(express.json()); 
+app.use(cors());
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -24,6 +27,7 @@ const __dirname = dirname(__filename);
 
 
 app.use('/api', FormRoute);
+app.use('/api',ResponseForm);
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Something went wrong!' });
