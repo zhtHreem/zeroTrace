08a71746447d22d72e50a28c723299b4c0e91bd4
Hareem <zainabhareemtayyab@gmail.com>
2024-11-17 17:11:09 +0500

    added url

--- backend/src/database/connectivity.js
+++ backend/src/database/connectivity.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async ()=>{
  try{                                        //change it 
-    const mongoURI=process.env.MONGO_URI || //'mongodb+srv://admin:123@eventify.dkeujvr.mongodb.net/FamFest?retryWrites=true&w=majority&appName=eventify';
+    const mongoURI=process.env.MONGO_URI || 'mongodb+srv://admin:123@eventify.dkeujvr.mongodb.net/ZeroTrace?retryWrites=true&w=majority&appName=eventify';
     console.log("ok")
     await mongoose.connect(mongoURI);
     console.log('MongoDB connected...');
