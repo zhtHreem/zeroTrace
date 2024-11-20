d418a175ab31c66d27352bcec99aaafe8ebe6f74
afeefaKhaliq <afeefakhaliq045@gmail.com>
2024-11-20 21:52:45 +0500

    recaptcha

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,6 +8,7 @@ import NewForm from './Components/SurveyForm/newform';
 import FormPreview from './Components/ResponseForm/response';
 import { Navbar,Footer } from './Components/HomePage/navbar';
 import Header from './Components/HomePage/header';
+import Recap from './Components/Recapcha/recapcha';
 const router = createBrowserRouter([
   {
     path: "/login",
@@ -16,6 +17,10 @@ const router = createBrowserRouter([
   {
     path:"/createform",
     element:<NewForm/>
+  },
+  {
+    path: "/recapcha",
+    element: <Recap />
   },
   {
     path:"/form",
