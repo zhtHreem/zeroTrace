3e6f3e62178b319375a4ab01f2de95d1d0750d2f
Hareem <zainabhareemtayyab@gmail.com>
2025-01-16 02:00:39 +0500

    removed

--- frontend/src/Components/Surveys/survey.js
+++ frontend/src/Components/Surveys/survey.js
@@ -1,6 +1,5 @@
 import React from "react";
 import FormCards from "../HomePage/Cardform/card";
-import "../HomePage/Cardform/card.css"
 import { Navbar,Footer } from "../HomePage/navbar";
 
 function Survey (){
